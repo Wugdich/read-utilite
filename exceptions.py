1c36b8c52141b680e07ba91ff555484f4d0b8046
Alexander Ryabishkin <sintes4ek@gmail.com>
2022-08-10 20:27:40 +0300

    Add exceptions module.

---
+++ exceptions.py
@@ -0,0 +1,11 @@
+class CantFindTitle(Exception):
+    """Can't find title in BeautifulSoup object."""
+
+
+class CantFindFirstParagraph(Exception):
+    """Can't find first paragraph of the article in BeautifulSoup object."""
+
+
+class CantFindContents(Exception):
+    """Can't find contents of the article in BeautifulSoup object."""
+
